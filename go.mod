914093d09cfbfda48af7e509d3106651b523bdc3
Kyle Stanfield <kylesonfon@gmail.com>
2026-03-19 02:17:42 -0700

    Implement handleQuery

--- go.mod
+++ go.mod
@@ -41,7 +41,7 @@ require (
 	golang.org/x/text v0.25.0 // indirect
 	golang.org/x/tools v0.22.0 // indirect
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
-	google.golang.org/genai v1.50.0 // indirect
+	google.golang.org/genai v1.51.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/grpc v1.66.2 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
--- src/main.go
+++ src/main.go
@@ -5,8 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/apache/arrow/go/v17/arrow"
@@ -15,15 +18,23 @@ import (
 	"github.com/lancedb/lancedb-go/pkg/contracts"
 	"github.com/lancedb/lancedb-go/pkg/lancedb"
 	allminilm "kylestanfield.com/shelllm/src/internal/all_minilm"
+
+	"google.golang.org/genai"
 )
 
+// IPC Consts
 const historySocket string = "/tmp/shelllm.history.socket"
 const bufferSize int = 4096
-
 const querySocket string = "/tmp/shelllm.query.socket"
 
+// VectorDB Consts
 const dbPath string = "/tmp/testdb"
 const tableName string = "CommandHistory"
+const vectorColumnName string = "vector"
+const kMostSimilar int = 5
+
+// Gemini consts
+const systemPrompt string = "You are answering user queries from a command line tool that tracks bash command history and output for added contest. The command context is:\n"
 
 // all mini llm has 384 dimension embeddings
 const EmbeddingDimensions int = 384
@@ -35,13 +46,22 @@ func openOrCreateDatabase(ctx context.Context) (contracts.ITable, *arrow.Schema,
 		return nil, nil, err
 	}
 
+	vectorMetadata := arrow.NewMetadata(
+		[]string{"lancedb:metric"},
+		[]string{"cosine"},
+	)
+
 	fields := []arrow.Field{
 		{Name: "id", Type: arrow.PrimitiveTypes.Int32, Nullable: false},
 		{Name: "command", Type: arrow.BinaryTypes.String, Nullable: false},
 		{Name: "output", Type: arrow.BinaryTypes.String, Nullable: false},
 		{Name: "returncode", Type: arrow.PrimitiveTypes.Int32, Nullable: false},
-		{Name: "vector", Type: arrow.FixedSizeListOf(int32(EmbeddingDimensions),
-			arrow.PrimitiveTypes.Float32), Nullable: false},
+		{
+			Name:     vectorColumnName,
+			Type:     arrow.FixedSizeListOf(int32(EmbeddingDimensions), arrow.PrimitiveTypes.Float32),
+			Nullable: false,
+			Metadata: vectorMetadata,
+		},
 	}
 	arrowSchema := arrow.NewSchema(fields, nil)
 
@@ -76,6 +96,10 @@ type CommandResult struct {
 	ReturnCode int
 }
 
+func (c CommandResult) String() string {
+	return c.Command + " " + c.Output
+}
+
 func SaveCommandResult(ctx context.Context,
 	res CommandResult,
 	embedding []float32,
@@ -108,8 +132,8 @@ func SaveCommandResult(ctx context.Context,
 	return table.Add(ctx, record, nil)
 }
 
-func GenerateEmbedding(ctx context.Context, result CommandResult, model *allminilm.Model) ([]float32, error) {
-	return model.Compute(result.Command+" "+result.Output, false)
+func GenerateEmbedding(ctx context.Context, sentence string, model *allminilm.Model) ([]float32, error) {
+	return model.Compute(sentence, false)
 }
 
 func ReadAndHandleCommand(ctx context.Context,
@@ -135,7 +159,7 @@ func ReadAndHandleCommand(ctx context.Context,
 	fmt.Printf("Read command over connection: %+v\n", res)
 	// Call the vector embedding library to get the vector for
 	// command result
-	cmdEmbedding, err := GenerateEmbedding(ctx, res, model)
+	cmdEmbedding, err := GenerateEmbedding(ctx, res.String(), model)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to generate embedding for %+v\n%v\n", res, err)
 		return err
@@ -177,11 +201,77 @@ func handleConnection(ctx context.Context,
 	}
 }
 
-func handleQuery(ctx context.Context, conn net.Conn, model *allminilm.Model, table contracts.ITable) {
+func parseResultToCommand(resultMap []map[string]interface{}) []CommandResult {
+	slice := make([]CommandResult, kMostSimilar)
+	for _, row := range resultMap {
+		res := CommandResult{
+			Command:    row["command"].(string),
+			Output:     row["output"].(string),
+			ReturnCode: row["returncode"].(int),
+		}
+		slice = append(slice, res)
+	}
+	return slice
+}
+
+func createContext(commands []CommandResult) string {
+	var contextBuilder strings.Builder
+
+	for i, row := range commands {
+		var b strings.Builder
+		b.WriteString("Command ")
+		b.WriteString(strconv.Itoa(i))
+		b.WriteString(":\n User Command:")
+		b.WriteString(row.Command)
+		b.WriteString("\n Command Output:")
+		b.WriteString(row.Output)
+		b.WriteString("\n rc: ")
+		b.WriteString(strconv.Itoa(row.ReturnCode))
+		b.WriteString("\n")
+		contextBuilder.WriteString(b.String())
+	}
+	return contextBuilder.String()
+}
+
+func handleQuery(ctx context.Context,
+	conn net.Conn, model *allminilm.Model,
+	table contracts.ITable,
+	gemini *genai.Client) {
+
 	// Read user query
 	buf := make([]byte, 2048)
 	bytesRead, err := conn.Read(buf)
+	if err != nil || bytesRead <= 0 {
+		fmt.Fprintf(os.Stderr, "Failed to read from unix socket: err=%v , n=%d", err, bytesRead)
+		return
+	}
+	userQuery := string(buf)
+	fmt.Println("Received user query %s", userQuery)
 
+	// generate embedding for query
+	embedding, err := GenerateEmbedding(ctx, userQuery, model)
+
+	resultMap, err := table.VectorSearch(ctx, vectorColumnName, embedding, kMostSimilar)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to search the vectorDB: %v", err)
+		return
+	}
+	cmdSlice := parseResultToCommand(resultMap)
+	geminiResponse, err := gemini.Models.GenerateContent(
+		ctx,
+		"gemini-3.1-flash",
+		genai.Text(systemPrompt+createContext(cmdSlice)+" The user query is: "+userQuery),
+		nil,
+	)
+	if err != nil {
+		log.Println("Failed to query Gemini gemini-3.1-flash")
+		return
+	}
+	if len(geminiResponse.Candidates) > 0 && len(geminiResponse.Candidates[0].Content.Parts) > 0 {
+		conn.Write([]byte(geminiResponse.Candidates[0].Content.Parts[0].Text))
+		return
+	}
+	log.Println("Malformed response from Gemini")
 }
 
 func main() {
@@ -190,7 +280,7 @@ func main() {
 	// 0. setup all-MiniLM-L6-v2 model for sentence embeddings
 	model, err := allminilm.NewModel()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	fmt.Println("Successfully loaded embedding model")
 	defer model.Close()
@@ -198,10 +288,19 @@ func main() {
 	// create it if it doesn't exist
 	table, schema, err := openOrCreateDatabase(ctx)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to open database: %v\n", err)
+		log.Fatal(err)
 	}
 
-	// 2. create Unix domain sockets for IPC
+	// 2. Setup Gemini connection
+	geminiClient, err := genai.NewClient(ctx, &genai.ClientConfig{
+		APIKey:  os.Getenv("GEMINI_API_KEY"),
+		Backend: genai.BackendGeminiAPI,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 3. create Unix domain sockets for IPC
 	// one socket to receive command history from user's shell,
 	// and another socket for handling queries
 	_ = os.Remove(historySocket)
@@ -210,9 +309,9 @@ func main() {
 	//
 	historyListener, err := net.Listen("unix", historySocket)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to create Unix socket: %v\n", err)
+		log.Fatal(err)
 	}
-	// 3. listen for command history
+	// 4. listen for command history
 	go func() {
 		fmt.Printf("Listening for history on socket %s\n", historySocket)
 		for {
@@ -225,18 +324,15 @@ func main() {
 		}
 	}()
 
-	// 4. listen for user queries
+	// 5. listen for user queries
 	queryListener, _ := net.Listen("unix", querySocket)
-	fmt.Println("Listening for queris on socket %s", querySocket)
-
+	fmt.Printf("Listening for queries on socket %s", querySocket)
 	for {
 		conn, err := queryListener.Accept()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to accept query connection: %v\n", err)
 			continue
 		}
-		// TODO implement handlequery to use RAG to get context from command history
-		// and pass that and the query to an LLM
-		go handleQuery(ctx, conn, model, table)
+		go handleQuery(ctx, conn, model, table, geminiClient)
 	}
 }
